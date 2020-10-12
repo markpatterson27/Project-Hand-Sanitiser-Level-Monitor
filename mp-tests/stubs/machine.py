312191e0d09ed5da74b763efbb25a6c5b8ca2eaa
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-10-12 07:22:45 +0100

    added RTC class to machine stub

--- mp-tests/stubs/machine.py
+++ mp-tests/stubs/machine.py
@@ -145,3 +145,16 @@ class TouchPad:
 
     def read(self):
         return 200
+
+class RTC:
+
+    def __init__ (self):
+        self._memory = b''
+
+    @property
+    def memory(self):
+        return self._memory
+
+    @memory.setter
+    def memory(self, memory):
+        self._memory = memory
